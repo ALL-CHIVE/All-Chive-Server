56d3b8cedc6d6a7e4fedb5db070e03723c346761
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-06-27 00:55:27 +0900

    [feat] 스웨거 셋팅, security 기본 셋팅 #1

---
+++ Api/src/main/java/allchive/server/api/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package allchive.server.api.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(ServletContext servletContext) {
+        String contextPath = servletContext.getContextPath();
+        Server server = new Server().url(contextPath);
+        return new OpenAPI().servers(List.of(server)).components(authSetting()).info(swaggerInfo());
+    }
+
+    private Info swaggerInfo() {
+        License license = new License();
+        license.setUrl("https://github.com/Central-MakeUs");
+        license.setName("Dachive");
+
+        return new Info()
+                .version("v0.0.1")
+                .title("\"Dachive 서버 API문서\"")
+                .description("Dachive 서버의 API 문서 입니다.")
+                .license(license);
+    }
+
+    private Components authSetting() {
+        return new Components()
+                .addSecuritySchemes(
+                        "access-token",
+                        new SecurityScheme()
+                                .type(Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(In.HEADER)
+                                .name("Authorization"));
+    }
+}
