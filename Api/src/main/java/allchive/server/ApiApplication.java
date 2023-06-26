0f4f479f3990e65d675dcbbf74b98cc5ba568148
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-06-26 13:41:01 +0900

    [feat] init

---
+++ Api/src/main/java/allchive/server/ApiApplication.java
@@ -0,0 +1,13 @@
+package allchive.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
