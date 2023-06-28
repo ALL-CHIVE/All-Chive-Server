f7c46577a09b8b8bafc8c38edb199db1c2b1a978
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-06-28 22:28:58 +0900

    [feat] block entity 설계 #5

---
+++ Domain/src/main/java/allchive/server/domain/domains/block/domain/Block.java
@@ -0,0 +1,32 @@
+package allchive.server.domain.domains.block.domain;
+
+import allchive.server.domain.common.model.BaseTimeEntity;
+import allchive.server.domain.domains.block.domain.enums.BlockType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "tbl_block")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Block extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // block 한 사람
+    @NotNull private Long blockFrom;
+
+    @Enumerated(EnumType.STRING)
+    private BlockType blockType;
+
+    // Block 당한 유저
+    private Long blockUser;
+
+    // Block 당한 컨텐츠
+    private Long blockContent;
+
+}
---
+++ Domain/src/main/java/allchive/server/domain/domains/block/repository/BlockRepository.java
@@ -0,0 +1,7 @@
+package allchive.server.domain.domains.block.repository;
+
+import allchive.server.domain.domains.block.domain.Block;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockRepository extends JpaRepository<Block, Long> {
+}
