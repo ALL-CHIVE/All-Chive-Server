661ab377a9e398c509876889a1fd1cea422e09db
Sanghoon Jeong <67852689+wjdtkdgns@users.noreply.github.com>
2023-06-29 01:13:13 +0900

    [feat] redis 셋팅, redisson 셋팅 #6 (#8)

---
+++ Infrastructure/src/main/java/allchive/server/infrastructure/config/redis/RedisConfig.java
@@ -0,0 +1,45 @@
+package allchive.server.infrastructure.config.redis;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import java.time.Duration;
+
+@EnableRedisRepositories(
+        basePackages = "allchive.server",
+        enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+
+        if (redisPassword != null && !redisPassword.isBlank())
+            redisConfig.setPassword(redisPassword);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .commandTimeout(Duration.ofSeconds(1))
+                        .shutdownTimeout(Duration.ZERO)
+                        .build();
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
+}
