f33ea3b8de087b8629b5fd9897cf6974ee124677
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-07-02 20:31:12 +0900

    [feat] logout 구현 #12

--- Api/src/main/java/allchive/server/api/auth/controller/authController.java
+++ Api/src/main/java/allchive/server/api/auth/controller/authController.java
@@ -1,16 +1,14 @@
 package allchive.server.api.auth.controller;
 
 
+import allchive.server.api.auth.service.LogOutUserUseCase;
 import allchive.server.api.auth.service.WithdrawUserUseCase;
 import allchive.server.domain.domains.user.domain.enums.OauthProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "1-1. [auth]")
 public class authController {
     private final WithdrawUserUseCase withdrawUserUseCase;
+    private final LogOutUserUseCase logOutUserUseCase;
 
     @Operation(summary = "회원탈퇴를 합니다.")
     @DeleteMapping("/withdrawal/{provider}")
     public void withDrawUser(@PathVariable OauthProvider provider) {
         withdrawUserUseCase.execute(provider);
+    }
+
+    @Operation(summary = "로그아웃을 합니다.")
+    @PostMapping("/logout")
+    public void logOutUser() {
+        logOutUserUseCase.execute();
     }
 }
