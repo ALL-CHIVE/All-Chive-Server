1b6a6c5e9b6d9731f08f1890ecf4b5c4f30cc3f8
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-07-18 11:19:15 +0900

    [feat] 연관 검색어 조회 기능 구현 #38

--- Api/src/main/java/allchive/server/api/search/controller/SearchController.java
+++ Api/src/main/java/allchive/server/api/search/controller/SearchController.java
@@ -6,6 +6,7 @@ import allchive.server.api.search.model.dto.response.SearchListResponse;
 import allchive.server.api.search.model.dto.response.SearchResponse;
 import allchive.server.api.search.model.enums.ArchivingType;
 import allchive.server.api.search.service.GetLatestSearchListUseCase;
+import allchive.server.api.search.service.GetRelativeSearchListUseCase;
 import allchive.server.api.search.service.SearchArchivingUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -24,9 +25,10 @@ import org.springframework.web.bind.annotation.*;
 public class SearchController {
     private final SearchArchivingUseCase searchArchivingUseCase;
     private final GetLatestSearchListUseCase getLatestSearchListUseCase;
+    private final GetRelativeSearchListUseCase getRelativeSearchListUseCase;
 
     @Operation(summary = "검색어를 검색합니다.")
-    @PostMapping()
+    @PostMapping
     public SearchResponse searchArchiving(
             @ParameterObject @PageableDefault(size = 10) Pageable pageable,
             @RequestParam("type") ArchivingType type,
@@ -35,8 +37,14 @@ public class SearchController {
     }
 
     @Operation(summary = "최근 검색어 목록을 가져옵니다.", description = "5개만 드릴게요")
-    @GetMapping()
+    @GetMapping(value = "/latest")
     public SearchListResponse getLatestSearchList() {
         return getLatestSearchListUseCase.execute();
+    }
+
+    @Operation(summary = "검색어 자동 완성")
+    @PostMapping(value = "/relation")
+    public SearchListResponse getRelativeSearchList(@RequestBody SearchRequest request) {
+        return getRelativeSearchListUseCase.execute(request);
     }
 }
---
+++ Api/src/main/java/allchive/server/api/search/service/GetRelativeSearchListUseCase.java
@@ -0,0 +1,45 @@
+package allchive.server.api.search.service;
+
+import static allchive.server.core.consts.AllchiveConst.SEARCH_KEY;
+import static jodd.util.StringPool.ASTERISK;
+
+import allchive.server.api.search.model.dto.request.SearchRequest;
+import allchive.server.api.search.model.dto.response.SearchListResponse;
+import allchive.server.core.annotation.UseCase;
+import allchive.server.domain.domains.user.adaptor.UserAdaptor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+
+@UseCase
+@RequiredArgsConstructor
+public class GetRelativeSearchListUseCase {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public SearchListResponse execute(SearchRequest request) {
+        HashOperations<String, String, Long> hashOperations = redisTemplate.opsForHash();
+        ScanOptions scanOptions = getScanOptions(request.getKeyword());
+        Cursor<Entry<String, Long>> cursor = hashOperations.scan(SEARCH_KEY, scanOptions);
+        return SearchListResponse.from(getRelationList(cursor));
+    }
+
+    private ScanOptions getScanOptions(String keyword) {
+        return ScanOptions.scanOptions().match(keyword + ASTERISK).build();
+    }
+
+    private List<String> getRelationList(Cursor<Entry<String, Long>> cursor) {
+        List<String> searchList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            if (cursor.hasNext()) {
+                Entry<String, Long> entry = cursor.next();
+                searchList.add(entry.getKey());
+            }
+        }
+        return searchList;
+    }
+}
