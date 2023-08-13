fea01032d17d452f630a5294b5ec866120c46731
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-08-13 18:11:02 +0900

    [fix] 삭제 컨텐츠 복구시 archiving count 변경 로직 추가 #93

--- Api/src/main/java/allchive/server/api/recycle/service/RestoreDeletedObjectUseCase.java
+++ Api/src/main/java/allchive/server/api/recycle/service/RestoreDeletedObjectUseCase.java
@@ -6,12 +6,19 @@ import allchive.server.api.recycle.model.dto.request.RestoreDeletedObjectRequest
 import allchive.server.core.annotation.UseCase;
 import allchive.server.domain.domains.archiving.service.ArchivingDomainService;
 import allchive.server.domain.domains.archiving.validator.ArchivingValidator;
+import allchive.server.domain.domains.content.adaptor.ContentAdaptor;
+import allchive.server.domain.domains.content.domain.Content;
 import allchive.server.domain.domains.content.service.ContentDomainService;
 import allchive.server.domain.domains.content.validator.ContentValidator;
 import allchive.server.domain.domains.recycle.service.RecycleDomainService;
 import allchive.server.domain.domains.recycle.validator.RecycleValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static allchive.server.core.consts.AllchiveConst.MINUS_ONE;
+import static allchive.server.core.consts.AllchiveConst.PLUS_ONE;
 
 @UseCase
 @RequiredArgsConstructor
@@ -22,6 +29,7 @@ public class RestoreDeletedObjectUseCase {
     private final ArchivingDomainService archivingDomainService;
     private final ContentDomainService contentDomainService;
     private final RecycleDomainService recycleDomainService;
+    private final ContentAdaptor contentAdaptor;
 
     @Transactional
     public void execute(RestoreDeletedObjectRequest request) {
@@ -29,8 +37,17 @@ public class RestoreDeletedObjectUseCase {
         validateExecution(request, userId);
         archivingDomainService.restoreByIdIn(request.getArchivingIds());
         contentDomainService.restoreByIdIn(request.getContentIds());
+        increaseArchivingCount(request.getContentIds());
         recycleDomainService.deleteAllByUserIdAndArchivingIdInOrUserIdAndContentIdIn(
                 request.getArchivingIds(), request.getContentIds(), userId);
+    }
+
+    private void increaseArchivingCount(List<Long> contentIds) {
+        contentAdaptor.findAllByIdIn(contentIds)
+                        .forEach(content ->
+                                archivingDomainService.updateContentCnt(
+                                        content.getArchivingId(), content.getContentType(), PLUS_ONE));
+
     }
 
     private void validateExecution(RestoreDeletedObjectRequest request, Long userId) {
