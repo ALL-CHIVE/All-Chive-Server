7ac59c3c6f43dcb4b58f2bf55fe20036de241f8a
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-08-13 18:04:00 +0900

    [feat] content 삭제시 archiving count 수정 로직 추가 #93

--- Api/src/main/java/allchive/server/api/content/service/DeleteContentUseCase.java
+++ Api/src/main/java/allchive/server/api/content/service/DeleteContentUseCase.java
@@ -4,6 +4,9 @@ package allchive.server.api.content.service;
 import allchive.server.api.config.security.SecurityUtil;
 import allchive.server.api.recycle.model.mapper.RecycleMapper;
 import allchive.server.core.annotation.UseCase;
+import allchive.server.domain.domains.archiving.service.ArchivingDomainService;
+import allchive.server.domain.domains.content.adaptor.ContentAdaptor;
+import allchive.server.domain.domains.content.domain.Content;
 import allchive.server.domain.domains.content.service.ContentDomainService;
 import allchive.server.domain.domains.content.validator.ContentValidator;
 import allchive.server.domain.domains.recycle.domain.Recycle;
@@ -12,6 +15,9 @@ import allchive.server.domain.domains.recycle.service.RecycleDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
+import static allchive.server.core.consts.AllchiveConst.MINUS_ONE;
+import static allchive.server.core.consts.AllchiveConst.PLUS_ONE;
+
 @UseCase
 @RequiredArgsConstructor
 public class DeleteContentUseCase {
@@ -19,6 +25,8 @@ public class DeleteContentUseCase {
     private final ContentDomainService contentDomainService;
     private final RecycleMapper recycleMapper;
     private final RecycleDomainService recycleDomainService;
+    private final ArchivingDomainService archivingDomainService;
+    private final ContentAdaptor contentAdaptor;
 
     @Transactional
     public void execute(Long contentId) {
@@ -26,6 +34,7 @@ public class DeleteContentUseCase {
         validateExecution(contentId, userId);
         contentDomainService.softDeleteById(contentId);
         createRecycle(userId, contentId);
+        decreaseArchivingCount(contentId);
     }
 
     private void validateExecution(Long contentId, Long userId) {
@@ -37,5 +46,12 @@ public class DeleteContentUseCase {
         Recycle recycle =
                 recycleMapper.toContentRecycleEntity(userId, contentId, RecycleType.CONTENT);
         recycleDomainService.save(recycle);
+    }
+
+    private void decreaseArchivingCount(Long contentId) {
+        Content content = contentAdaptor.findById(contentId);
+        archivingDomainService.updateContentCnt(
+                content.getArchivingId(), content.getContentType(), MINUS_ONE);
+
     }
 }
